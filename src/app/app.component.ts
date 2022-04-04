513da502ec60321ffd5ee8c8958ea0251ac6f8de
mohammadiyan <email.emok@gmail.com>
2022-04-04 18:35:08 +0430

    finish v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+const electron = (<any>window).require('electron');
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ipc = electron.ipcRenderer;
+  pageindex: number = 0;
   title = 'emok-task-manager';
+  info: any = {};
+  processes: any[] = [];
+
+  constructor(private ngZone: NgZone, private messageService: MessageService) {
+    
+    this.ipc.on('info', (ev: any, resp: any) => {
+      this.ngZone.run(() => { this.info = resp })
+    })
+    this.ipc.on('getProcess', (ev: any, resp: any) => {
+      this.ngZone.run(() => { this.processes = resp })
+    })
+    this.ipc.on('kill', (ev: any, resp: any) => {
+      if(resp.ok){
+        this.messageService.add({
+          severity:'success',
+          summary:'عملیات موفق',
+          detail:'پردازش مورد نظر متوقف گردید.'
+        })
+      }else{
+        let msg:string = '';
+        (resp.errors).forEach((err:string) => {
+          msg+=err
+        });
+        this.messageService.add({
+          severity:'success',
+          summary:'خطای سیستمی',
+          detail:msg
+        })
+      }
+      
+    })
+  }
+
+  ngOnInit(): void {
+    this.ipc.send('info');
+    this.ipc.send('getProcess');
+    setInterval(() => {
+      this.ipc.send('getProcess')
+    }, 3000);
+  }
+
+  killProcess(id: string | number) {
+    this.ipc.send('kill', Number(id))
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {TabViewModule} from 'primeng/tabview';
+import {DividerModule} from 'primeng/divider';
+import {TableModule} from 'primeng/table';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import {DialogModule} from 'primeng/dialog';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule,
+    DividerModule,
+    TableModule,
+    SelectButtonModule,
+    ButtonModule,
+    ToastModule,
+    DialogModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
